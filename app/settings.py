495f8740280b66d16a62ebfbdd58fa1687bb5f50
Alena <golovleva24092001@mail.ru>
2024-09-01 12:47:04 +0300

    финальная

--- app/settings.py
+++ app/settings.py
@@ -86,7 +86,7 @@ WSGI_APPLICATION = 'app.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'home',
+        'NAME': 'home2',
         'USER': 'postgres',
         'PASSWORD': 'home',
         'HOST': 'localhost',
